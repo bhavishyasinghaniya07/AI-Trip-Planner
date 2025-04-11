42cc215d2c99372494dfe6534bc0800c37115e1b
bhavishyasinghaniya07 <parmarbhavishya@gmail.com>
2025-04-11 12:29:35 +0530

    Update testomonials.jsx

--- src/components/custom/testomonials.jsx
+++ src/components/custom/testomonials.jsx
@@ -1,11 +1,11 @@
 export const testimonials = [
   {
-    name: "David Jordan",
+    name: "Amit Rajput",
     role: "Digital nomad",
     image:
-      "https://tripplanner.ai/_next/image?url=%2Flanding%2Fimages%2Freviews%2Fdavid_jordan.webp&w=256&q=75",
+      "https://imgs.search.brave.com/gWojzx39SYVAlKjt7bUZFQNx5LSVo42dNp5ejJRai18/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMuaHVkbC5jb20v/dXNlcnMvcHJvZC8x/NDUzNTUyOV9jMWY1/ZmZiNTIxNzU0YWU4/YTA5YTlmMWQzMWUz/MTM1Yy5qcGc",
     rating: 5,
-    text: "Trip Planner AI saves time and stress by aiding travel planning, relieving indecision or uncertainty.",
+    text: "TripGenie saves time and stress by aiding travel planning, relieving indecision or uncertainty.",
     bgColor: "bg-purple-50",
     borderColor: "border-purple-200",
   },
@@ -15,15 +15,15 @@ export const testimonials = [
     image:
       "https://tripplanner.ai/_next/image?url=%2Flanding%2Fimages%2Freviews%2Ftushar.webp&w=256&q=75",
     rating: 5,
-    text: "Trip Planner AI offers diverse planning options in a user-friendly interface. Simplifies travel planning for enthusiasts.",
+    text: "TripGenie offers diverse planning options in a user-friendly interface. Simplifies travel planning for enthusiasts.",
     bgColor: "bg-green-50",
     borderColor: "border-green-200",
   },
   {
-    name: "Steve J",
+    name: "Sanchet Mishra",
     role: "Student",
     image:
-      "https://tripplanner.ai/_next/image?url=%2Flanding%2Fimages%2Freviews%2Fsteve_j.webp&w=256&q=75",
+      "https://imgs.search.brave.com/aP2htSNLmNXtdTiPgj1FUwDYAhlSTiergH2tlDUI2-M/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9zaGls/cGFyYXZlbGxhLmNv/bS93cC1jb250ZW50/L3VwbG9hZHMvMjAy/Mi8wNS9QaG90bzU1/d2ViLmpwZw",
     rating: 5,
     text: "I love traveling but hate planning. This app quickly organizes trip agendas, reducing decision fatigue.",
     bgColor: "bg-pink-50",
